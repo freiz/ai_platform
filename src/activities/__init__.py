2456b25516691749c8b4f918ed214d5cb9c18abe
freiz <freizsu@gmail.com>
2024-12-06 16:14:24 -0800

    Add tests for llm_activity

--- src/activities/__init__.py
+++ src/activities/__init__.py
@@ -1,3 +1,4 @@
 from src.activities.activity import Activity, Parameter, ParamType
+from src.activities.llm_activity import LLMActivity
 
-__all__ = ['Activity', 'Parameter', 'ParamType']
+__all__ = ['Activity', 'Parameter', 'ParamType', 'LLMActivity']
